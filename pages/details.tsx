43108b48fc4772c90379a10ad42bafac4e5d295d
Artem <gigahod.mm@gmail.com>
2024-09-15 12:16:54 +0500

    дорбатал полностью приложение

--- pages/details.tsx
+++ pages/details.tsx
@@ -1,10 +1,8 @@
-import axios from 'axios';
+import Head from 'next/head';
 import { useRouter } from 'next/router';
-import React, { useEffect, useState } from 'react';
-
 
 const details = () => {
-   
+
     const router = useRouter()
 
 
@@ -105,60 +103,65 @@ const details = () => {
     ]
 
     return (
-        <div className="min-h-screen w-full flex justify-center items-center">
-            <div className="app_box relative flex flex-col items-start gap-16 w-[450px] h-full"
-                style={{ background: "linear-gradient(-60deg, rgb(73, 169, 239), rgb(71, 190, 224))" }}>
-                <img src="../bg-line1.png" className='absolute top-[200px] left-0' alt="" />
-                <img src="../bg-line2.png" className='absolute top-[80px] right-0' alt="" />
+        <>
+
+            <Head>
+                <title>Weather App - Details</title>
+            </Head>
+            <div className="main absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center w-full h-full max-w-full">
+                <div className="app_box relative h-full flex flex-col items-start gap-16 w-[450px] max-h-full" style={{ background: "linear-gradient(-60deg, rgb(73, 169, 239), rgb(71, 190, 224))" }}>
+                    <img src="../bg-line1.png" className='absolute top-[200px] left-0' alt="" />
+                    <img src="../bg-line2.png" className='absolute top-[80px] right-0' alt="" />
 
 
-                <div className='px-[30px] pt-[40px]'>
-                    <img onClick={() => router.push('/')} src="../back.svg" alt="back" className='cursor-pointer' />
-                </div>
+                    <div className='px-[30px] pt-[40px]'>
+                        <img onClick={() => router.push('/')} src="../back.svg" alt="back" className='cursor-pointer' />
+                    </div>
 
-                <div className='flex w-full max-w-full justify-between items-center px-[30px] text-white'>
-                    <h1 className='font-[900] text-[24px]' style={{ textShadow: "-2px 3px 1px rgba(0, 0, 0, 0.10)" }}>Today</h1>
-                    <p className='text-[18px] font-[400]' style={{ textShadow: "-2px 3px 1px rgba(0, 0, 0, 0.10)" }}>{new Date().toLocaleString('en-US', { month: 'short' })}, {new Date().getUTCDate()} </p>
-                </div>
-
-
-                <div className='grid grid-cols-5 gap-[15px] px-[20px] text-white'>
-                    {
-                        statickData.map(item => {
-                            return (
-                                <div key={item.id} className='item-weather flex flex-col items-center gap-[15px] p-[10px] transition-al'>
-                                    <span className='text-[18px] font-[400]' style={{ textShadow: "-2px 3px 1px rgba(0, 0, 0, 0.10)" }}>{item.gradus}</span>
-                                    <img src={item.img} alt="" />
-                                    <span className='text-[18px] font-[400]' style={{ textShadow: "-2px 3px 1px rgba(0, 0, 0, 0.10)" }}>{item.time}</span>
-                                </div>
-                            )
-                        })
-                    }
-                </div>
-
-                <div className='w-full max-w-full px-[30px] text-white'>
-                    <div className='w-full flex justify-between items-center'>
-                        <h1 className='text-[24px] font-[900]' style={{ textShadow: "-2px 3px 1px rgba(0, 0, 0, 0.10)" }}>Next Forecast</h1>
-                        <img src="../calendar.png" alt="" />
+                    <div className='flex w-full max-w-full justify-between items-center px-[30px] text-white'>
+                        <h1 className='font-[900] text-[24px]' style={{ textShadow: "-2px 3px 1px rgba(0, 0, 0, 0.10)" }}>Today</h1>
+                        <p className='text-[18px] font-[400]' style={{ textShadow: "-2px 3px 1px rgba(0, 0, 0, 0.10)" }}>{new Date().toLocaleString('en-US', { month: 'short' })}, {new Date().getUTCDate()} </p>
                     </div>
 
 
-                    <div className='box-weather flex w-full flex-col overflow-y-scroll h-[500px] pr-[50px]'>
+                    <div className='grid grid-cols-5 gap-[15px] px-[20px] text-white'>
+                        {
+                            statickData.map(item => {
+                                return (
+                                    <div key={item?.id} className='item-weather flex flex-col items-center gap-[15px] p-[10px] transition-all rounded-[10px]'>
+                                        <span className='text-[18px] font-[400]' style={{ textShadow: "-2px 3px 1px rgba(0, 0, 0, 0.10)" }}>{item?.gradus}</span>
+                                        <img src={item?.img} alt="" />
+                                        <span className='text-[18px] font-[400]' style={{ textShadow: "-2px 3px 1px rgba(0, 0, 0, 0.10)" }}>{item?.time}</span>
+                                    </div>
+                                )
+                            })
+                        }
+                    </div>
+
+                    <div className='w-full max-w-full px-[30px] text-white'>
+                        <div className='w-full flex justify-between items-center'>
+                            <h1 className='text-[24px] font-[900]' style={{ textShadow: "-2px 3px 1px rgba(0, 0, 0, 0.10)" }}>Next Forecast</h1>
+                            <img src="../calendar.png" alt="" />
+                        </div>
+
+
+                        <div className='box-weather flex flex-col gap-5 w-full max-w-full overflow-y-scroll h-[17rem] pr-[50px]'>
                         {
                             statickWeatherData.map(item => {
                                 return (
-                                    <div key={item.id} className='flex items-center justify-between'>
+                                    <div key={item?.id} className='flex items-center justify-between'>
                                         <span className='text-[18px] font-[700]' style={{ textShadow: "-2px 3px 1px rgba(0, 0, 0, 0.10)" }}>{item?.date}</span>
-                                        <img src={item.img} alt="" />
+                                        <img src={item?.img} alt="" />
                                         <span className='text-[18px] font-[400]' style={{ textShadow: "-2px 3px 1px rgba(0, 0, 0, 0.10)" }}>{item?.gradus}</span>
                                     </div>
                                 )
                             })
                         }
                     </div>
+                    </div>
                 </div>
-            </div>
-        </div >
+            </div >
+        </>
     );
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,39 +1,52 @@
 import { useState } from 'react';
 import { useRouter } from 'next/router';
 import axios from 'axios';
-
+import Head from 'next/head';
+import { useForm } from 'react-hook-form';
 
 
 export default function Home({ data }: { data: any }) {
     const [city, setCity] = useState('');
     const router = useRouter();
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+    } = useForm()
 
-    const handleSubmit = (e: React.FormEvent) => {
-        e.preventDefault();
+    const onSubmit = (data: any) => {
 
-        const cityQuery = city.trim() !== '' ? city : 'Samarkand';
-
+        setCity(data.query)
 
         router.push({
             pathname: '/',
-            query: { city: cityQuery },
+            query: { city: data.query },
         });
     };
 
 
     return (
         <>
+            <Head>
+                <title>Weather App - Home</title>
+            </Head>
             <div className="main absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center w-full h-full max-w-full">
                 <div className="app_box relative h-full flex flex-col items-center gap-16 w-[450px] max-h-full" style={{ background: "linear-gradient(-60deg, rgb(73, 169, 239), rgb(71, 190, 224))" }}>
                     <img src="../bg-line1.png" className='absolute top-[200px] left-0' alt="" />
                     <img src="../bg-line2.png" className='absolute top-[80px] right-0' alt="" />
                     <div className="top p-[30px] w-full">
-                        <div className='flex items-center justify-between w-full max-w-full h-[57px] bg-white rounded-2xl p-4'>
-                            <form onSubmit={handleSubmit} className='flex items-center gap-[20px]'>
-                                <button type='submit'><img className='cursor-pointer' src="../arrow.png" alt="" /></button>
-                                <input onChange={(e) => setCity(e.target.value)} value={city} className='bg-[none] border-none' type="text" placeholder='Search here' name='query' required />
+                            <div className={errors.query ? 'flex items-center justify-between w-full max-w-full h-[57px] border-[2px] border-[red] bg-white text-red-600 rounded-2xl p-4' : 'flex items-center border-[2px] justify-between w-full max-w-full h-[57px] bg-white rounded-2xl p-4'}>
+                            <form onSubmit={handleSubmit(onSubmit)} className='flex items-center gap-[20px] w-full justify-between'>
+                                <div className='flex items-center gap-2'>
+                                    <img className='cursor-pointer' src="../arrow.png" alt="" />
+                                    <input className='bg-[none] border-none' type="text" placeholder='Search here' {...register("query", { required: true, pattern: /^[a-z ,.'-]+$/i })}  />
+                                </div>
+                                <div className='flex items-items-center'>
+
+                                    <button type='submit'><img className='cursor-pointer w-[25px]' src="../search.png" alt="" /></button>
+                                </div>
                             </form>
-                            <img className='cursor-pointer' src="../micro.png" alt="" />
                         </div>
                     </div>
 
@@ -41,17 +54,17 @@ export default function Home({ data }: { data: any }) {
                         <img className='z-50 opacity-[11]' src="../sun.png" alt="" />
                     </div>
 
-                    <div className='w-full max-w-full px-[30px] min-h-ull '>
-                        <div onClick={() => router.push('/details')} className='border-[2px] p-[20px] cursor-pointer w-full max-w-full flex flex-col bg-[#80bcf8] items-center gap-[15px] border-gray-300 rounded-2xl'>
+                    <div className='w-full max-w-full h-full'>
+                        <div onClick={() => router.push('/details')} className='border-[2px] p-[20px] cursor-pointer w-full max-w-full flex flex-col bg-[#80bcf8] items-center gap-[20px] border-gray-300 rounded-2xl'>
                             <div className='pt-[10px] flex justify-center items-center text-[18px] text-white'>
                                 Today, {new Date().getUTCDate()} {new Date().toLocaleString('en-US', { month: 'long' })}
                             </div>
                             <div>
-                                <h1 className='font-[500] text-[80px] text-white'>{data.current.temperature}°</h1>
+                                <h1 className='font-[500] text-[80px] text-white'>{data?.current?.temperature}°</h1>
                             </div>
 
                             <div>
-                                <span className='text-[15px] font-[400] text-white'>{data.location.name}</span>
+                                <span className='text-[17px] font-[600] text-white'>{data?.location?.name}</span>
                             </div>
 
                             <div className='flex flex-col gap-[15px] items-start'>
@@ -62,7 +75,7 @@ export default function Home({ data }: { data: any }) {
                                     </div>
                                     <span className='text-white'>|</span>
                                     <div>
-                                        <span className='text-white text-[15px]'>{data.current.wind_speed}km/h</span>
+                                        <span className='text-white text-[15px]'>{data?.current?.wind_speed}km/h</span>
                                     </div>
                                 </div>
                                 <div className='flex justify-between gap-[20px] items-center'>
@@ -72,7 +85,7 @@ export default function Home({ data }: { data: any }) {
                                     </div>
                                     <span className='text-white'>|</span>
                                     <div>
-                                        <span className='text-white text-[15px]'>{data.current.humidity}%</span>
+                                        <span className='text-white text-[15px]'>{data.current?.humidity}%</span>
                                     </div>
                                 </div>
                             </div>
